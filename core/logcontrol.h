816e57fd724140b045df7ffb899a13b90b1f9333
Luiz Carlos Vieira <luigivieira@gmail.com>
2015-03-14 15:46:32 -0300

    updated documentation of LogControl class

--- core/logcontrol.h
+++ core/logcontrol.h
@@ -43,16 +43,59 @@ namespace f3
 		 */
 		virtual ~LogControl();
 
+		/**
+		 * Establishes a "connection" with the log control system. That basically
+		 * means to create or attach to the shared memory using for exchanging data.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 */
 		bool connect();
 
+		/**
+		 * Interrupts the "connection" with the log control system. That basically
+		 * means to dettach from the shared memory using for exchanging data.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 */
 		bool disconnect();
 
+		/**
+		 * Queries the current log level of the application with the given name.
+		 * @param sAppName QString with the application name to query.
+		 * @param eLevel Reference to a QtMsgType enumeration to receive the application current log level.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 * This method returns false if the log control has not been connected before or if
+		 * the application with the given name has not been registered yet (it is probably not
+		 * running).
+		 */
 		bool getLogLevel(const QString &sAppName, QtMsgType &eLevel);
 
+		/**
+		 * Updates the current log level of the application with the given name.
+		 * This method automatically registers the application in the log control by creating
+		 * an entry in the shared memory used to manage the data.
+		 * @param sAppName QString with the application name to update.
+		 * @param eLevel QtMsgType enumeration value with the new application log level.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 * This method returns false if the log control has not been connected before.
+		 */
 		bool setLogLevel(const QString &sAppName, const QtMsgType eLevel);
 
+		/**
+		 * Queries the map of all application names and their current log levels.
+		 * @param mAppLogData Reference to a QMap<QString, QtMsgType> to receive the
+		 * mapping of all existing (running) application names and their current log levels.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 * This method returns false if the log control has not been connected before.
+		 */
 		bool getAppLogData(QMap<QString, QtMsgType> &mAppLogData);
 
+		/**
+		 * Removes the entry of the given application name from the log control.
+		 * This method is supposed to be called by every application before it is closed.
+		 * @param sAppName QString with the application name to be removed from the shared memory.
+		 * @return Boolean indicating if the operation was successful (true) or not (false).
+		 * This method returns false if the log control has not been connected before. If the application
+		 * name does not exist already in the shared memory, the operation returns as successful.
+		 */
 		bool removeAppEntry(const QString &sAppName);
 
 	private:
